04142181c600886421b8dfb7605aaa1d0b4227c2
Spencer Elliott <me@elliottsj.com>
2014-10-26 23:28:26 -0400

    Add ask_question stub

--- pywatson/watson.py
+++ pywatson/watson.py
@@ -2,4 +2,18 @@ class Watson:
     """The Watson API adapter class"""
 
     def __init__(self, url, username, password):
+        self.url = url
+        self.username = username
+        self.password = password
+
+    def ask_question(self, question_text, question=None):
+        """Ask Watson a question via the Question and Answer API
+
+        :param question_text: question to ask Watson
+        :type question_text: str
+        :param question: if question_text is not provided, a Question object
+                         representing the question to ask Watson
+        :type question: Question
+        :return: Answer
+        """
         pass
