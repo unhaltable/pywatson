f3063afa3cd8284d7d9b7c6d805fd1d4dd315432
Spencer Elliott <me@elliottsj.com>
2014-10-26 23:28:17 -0400

    Add __init__ test

--- tests/test_watson.py
+++ tests/test_watson.py
@@ -3,6 +3,12 @@ from pywatson.watson import Watson
 
 
 class TestWatson:
+    def test___init__(self, config):
+        watson = Watson(url=config['url'], username=config['username'], password=config['password'])
+        assert watson.url == config['url']
+        assert watson.username == config['username']
+        assert watson.password == config['password']
+
     def test_ask_question_basic(self, watson):
         answer = watson.ask_question('What is the Labour Code?')
         assert type(answer) is Answer
