4a1804a49be7be55964f1e7dc7a3a9417df1f37b
Spencer Elliott <me@elliottsj.com>
2014-10-28 15:02:54 -0400

    Create EvidenceRequest constructor

--- pywatson/question/evidence_request.py
+++ pywatson/question/evidence_request.py
@@ -1,2 +1,9 @@
-class EvidenceRequest:
-    pass
+class EvidenceRequest(object):
+    """Include this with a Question to request evidence from Watson"""
+
+    def __init__(self, items=3, profile=False):
+        self.items = items
+        self.profile = profile
+
+    def __eq__(self, other):
+        return False
