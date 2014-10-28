894203d67e88e8bac8ec4f8948d940789387b648
Spencer Elliott <me@elliottsj.com>
2014-10-28 18:35:42 -0400

    Add all blank parameters to sample question

--- tests/data/questions.py
+++ tests/data/questions.py
@@ -3,6 +3,25 @@ QUESTIONS = [
         'questionText': 'What is the Labour Code?'
     },
     {
-        'questionText': 'When can a union start a strike?'
+        'questionText': 'When can a union start a strike?',
+        'items': 0,
+        'evidenceRequest': {
+            'items': 0,
+            'profile': ''
+        },
+        'answerAssertion': '',
+        'category': '',
+        'context': '',
+        'formattedAnswer': '',
+        'passthru': '',
+        'synonyms': '',
+        'lat': '',
+        'filters': [
+            {
+                'filterType': '',
+                'filterName': '',
+                'values': ''
+            }
+        ]
     }
 ]
