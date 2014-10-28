e3d4dbb67c2e4012f82851b94dbfc6525c9a465e
Spencer Elliott <me@elliottsj.com>
2014-10-27 22:29:17 -0400

    Create Question constructor

--- pywatson/question/question.py
+++ pywatson/question/question.py
@@ -1,3 +1,62 @@
-class Question(object):
+from pywatson.question.evidence_request import EvidenceRequest
+
+
+class Question:
     """A question to ask Watson"""
-    pass
+
+    def __init__(self,
+                 question_text,
+                 answer_assertion=None,
+                 category=None,
+                 context=None,
+                 evidence_request=None,
+                 filters=(),
+                 formatted_answer=None,
+                 items=None,
+                 lat=None,
+                 passthru=None,
+                 synonym_list=()):
+        """Create this Question with the given parameters.
+
+        See `IBM Watson Developer Cloud`_ for more details
+
+        .. _`IBM Watson Developer Cloud`: http://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/apis/#!/Question_Answer/question
+
+        :param question_text: the text of the question to be answered
+        :type question_text: str
+        :param answer_assertion:
+        :type answer_assertion: str
+        :param category:
+        :type category: str
+        :param context:
+        :type context: str
+        :param evidence_request:
+        :type evidence_request: EvidenceRequest
+        :param filters:
+        :type filters: tuple of Filter
+        :param formatted_answer:
+        :type formatted_answer: bool
+        :param items:
+        :type items: int
+        :param lat:
+        :type lat: str
+        :param passthru:
+        :type passthru: str
+        :param synonym_list:
+        :type synonym_list: tuple of str
+        :return: Question
+        """
+        self.question_text = question_text
+        self.answer_assertion = answer_assertion
+        self.category = category
+        self.context = context
+        self.evidence_request = evidence_request
+        self.filters = filters
+        self.formatted_answer = formatted_answer
+        self.items = items
+        self.lat = lat
+        self.passthru = passthru
+        self.synonym_list = synonym_list
+
+    def to_dict(self):
+        pass
