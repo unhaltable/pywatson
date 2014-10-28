7e4b3610d7fa28a3b10f0faa854f6c2185243126
Spencer Elliott <me@elliottsj.com>
2014-10-28 15:02:44 -0400

    Create Filter class

--- pywatson/question/filter.py
+++ pywatson/question/filter.py
@@ -1,2 +1,20 @@
-class Filter:
-    pass
+from enum import Enum
+
+
+class FilterType(Enum):
+    date_range_filter = 'dateRangeFilter'
+    metadata_filter = 'metadataFilter'
+    prefix_filter = 'prefixFilter'
+    query_filter = 'queryFilter'
+
+
+class Filter(object):
+    """Use a Filter to restrict answers using metadata"""
+
+    def __init__(self, filter_type, field_name, values=()):
+        self.filter_type = filter_type
+        self.field_name = field_name
+        self.values = values
+
+    def __eq__(self, other):
+        return False
