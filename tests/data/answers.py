8bcccee55e6b649507a5f64dd225955012d4d819
Spencer Elliott <me@elliottsj.com>
2014-10-29 17:31:51 -0400

    Add test answer

--- tests/data/answers.py
+++ tests/data/answers.py
@@ -1,3 +1,214 @@
 ANSWERS = [
-    {}
+    {
+        'question': {
+            'pipelineid': '153681347',
+            'qclasslist': [
+                {
+                    'value': 'DESCRIPTIVE'
+                },
+                {
+                    'value': 'FACTOID'
+                }
+            ],
+            'synonymList': [
+                {
+                    'partOfSpeech': 'noun',
+                    'synSet': [
+                        {
+                            'name': 'Wordnet_labor-noun-1',
+                            'synonym': [
+                                {
+                                    'isChosen': True,
+                                    'value': 'proletariat',
+                                    'weight': 1.0
+                                },
+                                {
+                                    'isChosen': True,
+                                    'value': 'labor',
+                                    'weight': 1.0
+                                }
+                            ]
+                        }
+                    ],
+                    'lemma': 'labour',
+                    'value': 'labour'
+                },
+                {
+                    'partOfSpeech': 'verb',
+                    'synSet': [
+                        {
+                            'name': 'Lemma_Expansion',
+                            'synonym': [
+                                {
+                                    'isChosen': True,
+                                    'value': 'be',
+                                    'weight': 1.0
+                                }
+                            ]
+                        }
+                    ],
+                    'lemma': 'be',
+                    'value': 'is'
+                },
+                {
+                    'partOfSpeech': 'noun',
+                    'synSet': [
+                        {
+                            'name': 'Wordnet_code-noun-1',
+                            'synonym': [
+                                {
+                                    'isChosen': True,
+                                    'value': 'codification',
+                                    'weight': 1.0
+                                }
+                            ]
+                        }
+                    ],
+                    'lemma': 'code',
+                    'value': 'code'
+                }
+            ],
+            'questionText': 'What is the Labour Code?',
+            'formattedAnswer': False,
+            'focuslist': [
+                {
+                    'value': 'What'
+                }
+            ],
+            'status': 'Complete',
+            'id': '32C4518E1542435A994320B933D98FEE',
+            'category': '',
+            'evidenceRequest': {
+                'items': -1,
+                'profile': 'NO'
+            },
+            'latlist': [
+                {
+                    'value': 'What'
+                }
+            ],
+            'passthru': '',
+            'items': 5,
+            'errorNotifications': [],
+            'answers': [
+                {
+                    'pipeline': 'Descriptive,TAO',
+                    'confidence': 0.01319,
+                    'id': 0,
+                    'text': '74093ED8A37A20AF8FEE35BA0E5852D - CanLII - Canada Labour Code, RSC 1985, c L-2 : Canada Labour Code'
+                },
+                {
+                    'pipeline': 'TAO',
+                    'confidence': 0.0118,
+                    'id': 1,
+                    'text': '74093ED8A37A20AD7F484626195C6C4 - CanLII - Canada Labour Code, RSC 1985, c L-2'
+                },
+                {
+                    'pipeline': 'Descriptive,TAO',
+                    'confidence': 0.00657,
+                    'id': 2,
+                    'text': 'AFB86D2942A7C1D4A96CAAE139DBE7C0 - CanLII - Labour Standards Code, RSNS 1989, c 246 : Labour Standards Code, RSNS 1989, c 246'
+                },
+                {
+                    'pipeline': 'TAO',
+                    'confidence': 0.00638,
+                    'id': 3,
+                    'text': '74093ED8A37A20A19493FDB294B607F - CanLII - Canada Labour Code, RSC 1985, c L-2 : INTERPRETATION'
+                },
+                {
+                    'pipeline': 'Descriptive,TAO',
+                    'confidence': 0.00589,
+                    'id': 4,
+                    'text': '74093ED8A37A20ACB601EF5E29BFD6D - CanLII - Canada Labour Code, RSC 1985, c L-2 : Canada Labour Code, RSC 1985, c L-2'
+                }
+            ],
+            'evidencelist': [
+                {
+                    'metadataMap': {
+                        'originalfile': 'CanLII - Canada Labour Code, RSC 1985, c L-2.html',
+                        'deepqaid': '33562483',
+                        'title': 'CanLII - Canada Labour Code, RSC 1985, c L-2 : Canada Labour Code',
+                        'corpusName': 'PB',
+                        'DOCNO': '74093ED8A37A20A251ED45580874251',
+                        'fileName': '/watsondata/ingestion/201410280224/eng/di/output/trec/intermediate/xml-splitTrecTrim/33562483.xml'
+                    },
+                    'copyright': 'No copyright specified for this supporting passage or document.',
+                    'id': 'PB_74093ED8A37A20A251ED45580874251',
+                    'termsOfUse': 'No license specified for this supporting passage or document.',
+                    'document': '/instance/507/deepqa/v1/question/document/PB_74093ED8A37A20A251ED45580874251/172/3783',
+                    'title': 'CanLII - Canada Labour Code, RSC 1985, c L-2 : Canada Labour Code',
+                    'text': 'R.S.C., 1985, c. L-2. An Act to consolidate certain statutes respecting labour.',
+                    'value': '0.013187802396714687'
+                },
+                {
+                    'metadataMap': {
+                        'originalfile': 'CanLII - Canada Labour Code, RSC 1985, c L-2.html',
+                        'fileName': '/watsondata/ingestion/201410280224/eng/di/output/trec/intermediate/xml-splitTrecTrim/33562469.xml',
+                        'title': 'CanLII - Canada Labour Code, RSC 1985, c L-2',
+                        'corpusName': 'PB',
+                        'DOCNO': '74093ED8A37A20A8BFC068C86165193',
+                        'CorpusPlusDocno': 'PB_74093ED8A37A20A8BFC068C86165193'
+                    },
+                    'copyright': 'http://en.wikipedia.org/wiki/Wikipedia:Copyrights',
+                    'id': 'T_74093ED8A37A20AD7F484626195C6C4',
+                    'termsOfUse': 'http://en.wikipedia.org/wiki/Wikipedia:Text_of_Creative_Commons_Attribution-ShareAlike_3.0_Unported_License',
+                    'document': '/instance/507/deepqa/v1/question/document/T_74093ED8A37A20AD7F484626195C6C4/0/-1',
+                    'title': 'CanLII - Canada Labour Code, RSC 1985, c L-2',
+                    'text': ' Francais. English. Home > Canada (Federal) > Statutes and Regulations > RSC 1985, c L-2.',
+                    'value': '0.011803392320871353'
+                },
+                {
+                    'metadataMap': {
+                        'originalfile': 'CanLII - Labour Standards Code, RSNS 1989, c 246.html',
+                        'deepqaid': '33562686',
+                        'title': 'CanLII - Labour Standards Code, RSNS 1989, c 246 : Labour Standards Code, RSNS 1989, c 246',
+                        'corpusName': 'PB',
+                        'DOCNO': 'AFB86D2942A7C1D46499E34AE969899A',
+                        'fileName': '/watsondata/ingestion/201410280224/eng/di/output/trec/intermediate/xml-splitTrecTrim/33562686.xml'
+                    },
+                    'copyright': 'No copyright specified for this supporting passage or document.',
+                    'id': 'PB_AFB86D2942A7C1D46499E34AE969899A',
+                    'termsOfUse': 'No license specified for this supporting passage or document.',
+                    'document': '/instance/507/deepqa/v1/question/document/PB_AFB86D2942A7C1D46499E34AE969899A/2982/3470',
+                    'title': 'CanLII - Labour Standards Code, RSNS 1989, c 246 : Labour Standards Code, RSNS 1989, c 246',
+                    'text': 'You may want to use these tools instead of the above search box. In force Construction and Property Maintenance, NS Reg 202/2003. General Labour Standards Code Regulations, NS Reg 298/90. Labour Standards Code Reciprocal Enforcement Regulations, NS Reg 154/95. Minimum Wage Order (General), NS Reg 5/99, Sch A. Minimum Wage Order (Logging and Forest Operations), NS Reg 5/99, Sch C. Repealed or Spent Minimum Wage Order (Road Building and Heavy Construction Industry), NS Reg 5/99, Sch B.',
+                    'value': '0.006566918920725584'
+                },
+                {
+                    'metadataMap': {
+                        'originalfile': 'CanLII - Canada Labour Code, RSC 1985, c L-2.html',
+                        'fileName': '/watsondata/ingestion/201410280224/eng/di/output/trec/intermediate/xml-splitTrecTrim/33562485.xml',
+                        'title': 'CanLII - Canada Labour Code, RSC 1985, c L-2 : INTERPRETATION',
+                        'corpusName': 'PB',
+                        'DOCNO': '74093ED8A37A20A7CD2BB8B5B5F9616',
+                        'CorpusPlusDocno': 'PB_74093ED8A37A20A7CD2BB8B5B5F9616'
+                    },
+                    'copyright': 'http://en.wikipedia.org/wiki/Wikipedia:Copyrights',
+                    'id': 'T_74093ED8A37A20A19493FDB294B607F',
+                    'termsOfUse': 'http://en.wikipedia.org/wiki/Wikipedia:Text_of_Creative_Commons_Attribution-ShareAlike_3.0_Unported_License',
+                    'document': '/instance/507/deepqa/v1/question/document/T_74093ED8A37A20A19493FDB294B607F/0/-1',
+                    'title': 'CanLII - Canada Labour Code, RSC 1985, c L-2 : INTERPRETATION',
+                    'text': ' Definitions. 2. In this Act, \'federal work, undertaking or business\'. << entreprises federales << \'federal work, undertaking or business\' means any work, undertaking or business that is within the legislative authority of Parliament, including, without restricting the generality of the foregoing, (a) a work, undertaking or business operated or carried on for or in connection with navigation and shipping, whether inland or maritime, including the operation of ships and transportation by ship anywhere in Canada, (b) a railway, canal, telegraph or other work or undertaking connecting any province with any other province, or extending beyond the limits of a province, (c) a line of ships connecting a province with any other province, or extending beyond the limits of a province, (d) a ferry between any province and any other province or between any province and any country other than Canada, (e) aerodromes, aircraft or a line of air transportation, (f) a radio broadcasting station, (g) a bank or an authorized foreign bank within the meaning of section 2 of the Bank Act, (h) a work or undertaking that, although wholly situated within a province, is before or after its execution declared by Parliament to be for the general advantage of Canada or for the advantage of two or more of the provinces, (i) a work, undertaking or business outside the exclusive legislative authority of the legislatures of the provinces, and. (j) a work, undertaking or activity in respect of which federal laws within the meaning of section 2 of the Oceans Act apply pursuant to section 20 of that Act and any regulations made pursuant to paragraph 26(1)(k) of that Act; \'Minister\'. << ministre << \'Minister\' means the Minister of Labour. R.S., 1985, c. L-2, s. 2; 1990, c. 44, s. 17; 1996, c. 31, s. 89; 1999, c. 28, s. 169.',
+                    'value': '0.006383867468684912'
+                },
+                {
+                    'metadataMap': {
+                        'originalfile': 'CanLII - Canada Labour Code, RSC 1985, c L-2.html',
+                        'deepqaid': '33562470',
+                        'title': 'CanLII - Canada Labour Code, RSC 1985, c L-2 : Canada Labour Code, RSC 1985, c L-2',
+                        'corpusName': 'PB',
+                        'DOCNO': '74093ED8A37A20A1B72BAB02090EB81',
+                        'fileName': '/watsondata/ingestion/201410280224/eng/di/output/trec/intermediate/xml-splitTrecTrim/33562470.xml'
+                    },
+                    'copyright': 'No copyright specified for this supporting passage or document.',
+                    'id': 'PB_74093ED8A37A20A1B72BAB02090EB81',
+                    'termsOfUse': 'No license specified for this supporting passage or document.',
+                    'document': '/instance/507/deepqa/v1/question/document/PB_74093ED8A37A20A1B72BAB02090EB81/4223/4561',
+                    'title': 'CanLII - Canada Labour Code, RSC 1985, c L-2 : Canada Labour Code, RSC 1985, c L-2',
+                    'text': 'On Board Trains Occupational Safety and Health Regulations, SOR/87-184. Ontario Hydro Nuclear Facilities Exclusion from Part III of the Canada Labour Code Regulations (Labour Standards), SOR/98-181. Ontario Hydro Nuclear Facilities Exclusion from Part II of the Canada Labour Code Regulations (Occupational Health and Safety), SOR/98-180.',
+                    'value': '0.0058912718668580055'
+                }
+            ]
+        }
+    }
 ]
