ca927a3391b894726be49312f330d53a1fdad536
Spencer Elliott <me@elliottsj.com>
2014-10-29 19:59:54 -0400

    Add WatsonAnswer constructor test

--- tests/test_answer.py
+++ tests/test_answer.py
@@ -1,3 +1,6 @@
+from pywatson.answer.answer import Answer
+from pywatson.answer.evidence import Evidence
+from pywatson.answer.synonym import Synonym
 from pywatson.answer.watson_answer import WatsonAnswer
 
 
@@ -9,3 +12,21 @@ class TestAnswer(object):
         answer = WatsonAnswer(answers[0])
 
         assert answer.raw == answers[0]
+        assert answer.id == '32C4518E1542435A994320B933D98FEE'
+        assert len(answer.answers) == 5
+        assert type(answer.answers[0]) is Answer
+        assert answer.category is None
+        assert answer.error_notifications == []  # TODO: add test data
+        assert len(answer.evidence_list) == 5
+        assert type(answer.evidence_list[0]) is Evidence
+        assert len(answer.focus_list) == 1
+        assert type(answer.focus_list[0]) is str
+        assert len(answer.lat_list) == 1
+        assert type(answer.lat_list[0]) is str
+        assert answer.pipelineid == '153681347'
+        assert len(answer.qclasslist) == 2
+        assert type(answer.qclasslist[0]) is str
+        assert answer.status == 'Complete'
+        assert answer.supplemental is None
+        assert len(answer.synonym_list) == 3
+        assert type(answer.synonym_list[0]) is Synonym
