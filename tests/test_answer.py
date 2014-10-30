e0cf3cdd960cd2bfad1c046ce62e942d24f54a84
Spencer Elliott <me@elliottsj.com>
2014-10-30 18:03:07 -0400

    Add more TestAnswer assertions

--- tests/test_answer.py
+++ tests/test_answer.py
@@ -1,4 +1,5 @@
 from pywatson.answer.answer import Answer
+from pywatson.answer.error_notification import ErrorNotification
 from pywatson.answer.evidence import Evidence
 from pywatson.answer.synonym import Synonym
 from pywatson.answer.watson_answer import WatsonAnswer
@@ -15,8 +16,11 @@ class TestAnswer(object):
         assert answer.id == '32C4518E1542435A994320B933D98FEE'
         assert len(answer.answers) == 5
         assert type(answer.answers[0]) is Answer
-        assert answer.category is None
-        assert answer.error_notifications == []  # TODO: add test data
+        assert answer.category is ''
+        assert len(answer.error_notifications) == 1
+        assert type(answer.error_notifications[0]) is ErrorNotification
+        assert answer.error_notifications[0].error is 'test error'
+        assert answer.error_notifications[0].text is 'test error text'
         assert len(answer.evidence_list) == 5
         assert type(answer.evidence_list[0]) is Evidence
         assert len(answer.focus_list) == 1
