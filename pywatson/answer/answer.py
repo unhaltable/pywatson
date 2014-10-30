572f27086b5c4921ce155e797d9bd5b7d271b9e9
Spencer Elliott <me@elliottsj.com>
2014-10-30 18:02:40 -0400

    Add Answer constructor

--- pywatson/answer/answer.py
+++ pywatson/answer/answer.py
@@ -1,2 +1,6 @@
 class Answer(object):
-    pass
+    def __init__(self, answer_mapping):
+        self.pipeline = answer_mapping['pipeline']
+        self.confidence = answer_mapping['confidence']
+        self.id = answer_mapping['id']
+        self.text = answer_mapping['text']
