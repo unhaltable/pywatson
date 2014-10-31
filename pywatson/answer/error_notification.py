c4e9e4782018c7174a443ad39fc48d00d8c68f0b
Spencer Elliott <me@elliottsj.com>
2014-10-31 05:43:03 -0400

    Add from_mapping() to ErrorNofitication

--- pywatson/answer/error_notification.py
+++ pywatson/answer/error_notification.py
@@ -1,5 +1,9 @@
 class ErrorNotification(object):
-    def __init__(self, error_mapping):
-        self.raw = error_mapping
-        self.error = error_mapping['error']
-        self.text = error_mapping['text']
+    def __init__(self, error, text):
+        self.error = error
+        self.text = text
+
+    @classmethod
+    def from_mapping(cls, error_mapping):
+        return cls(error=error_mapping['error'],
+                   text=error_mapping['text'])
